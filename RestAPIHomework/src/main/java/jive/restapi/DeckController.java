85c7e82e2a163bb60309eb4f1148a4c86f453966
Mahdi Malaki <malaki12003@gmail.com>
2019-05-26 23:38:31 +0430

    fix bugs

--- RestAPIHomework/src/main/java/jive/restapi/DeckController.java
+++ RestAPIHomework/src/main/java/jive/restapi/DeckController.java
@@ -30,28 +30,28 @@ public class DeckController {
 			return SUCCESSFUL_DELETE ;
 		}
 
-		@RequestMapping(method=RequestMethod.POST , value="/game/deck/player")
-		public String addDeck(@RequestParam(value="gameID") Long gameID, @RequestParam(value="deckID") Long deckID)
+		@RequestMapping(method=RequestMethod.POST , value="/game/{gameID}/deck/{deckID}")
+		public String addDeck(@PathVariable(value="gameID") Long gameID, @PathVariable(value="deckID") Long deckID)
 				throws DeckNotExistException, GameNotExistException, DeckAlreadyExistException {
 			gManager.addDeck(gameID, deckID);
 			return SUCCESSFUL_DECK_ADDED ;
 		}
 
-		@RequestMapping(method=RequestMethod.POST , value="/game")
+		@RequestMapping(method=RequestMethod.POST , value="/game/player")
 		public String addPlayer(@RequestParam(value="gameID") Long gameID, @RequestParam(value="playerID") Long playerID)
 				throws GameNotExistException, PlayerAlreadyExistException {
 			gManager.addPlayer(gameID, playerID);
 			return SUCCESSFUL_PLAYER_ADDED;
 		}
 
-		@RequestMapping(method=RequestMethod.DELETE , value="/game")
+		@RequestMapping(method=RequestMethod.DELETE , value="/game/player")
 		public String deletePlayer(@RequestParam(value="gameID") Long gameID, @RequestParam(value="playerID")
 			Long playerID) throws GameNotExistException, PlayerNotExistException, PlayerAlreadyExistException {
 			gManager.deletePlayer(gameID, playerID);
 			return SUCCESSFUL_PLAYER_DELETED;
 		}
 
-	    @RequestMapping(method=RequestMethod.GET , value="/game/deck/player")
+	    @RequestMapping(method=RequestMethod.GET , value="/game/deck")
 	    public Card dealCard(@RequestParam(value="gameID") Long gameID, @RequestParam(value="deckID") Long deckID,
 							 @RequestParam(value="playerID") Long playerID) throws DeckNotExistException,
 				NullDeckException, PlayerNotExistException, GameNotExistException {
@@ -59,42 +59,42 @@ public class DeckController {
 	        return retrievedCard;
 	    }
 
-		@RequestMapping(method=RequestMethod.GET , value="/game/deck/player")
+		@RequestMapping(method=RequestMethod.GET , value="/game/card")
 		public Hand getCards(@RequestParam(value="gameID") Long gameID, @RequestParam(value="playerID") Long playerID)
 				throws PlayerNotExistException, GameNotExistException {
 			Hand hand = gManager.getCards(gameID, playerID) ;
 			return hand;
 		}
 
-		@RequestMapping(method=RequestMethod.GET , value="/game")
+		@RequestMapping(method=RequestMethod.GET , value="/game/player")
 		public List<Player> getPlayers(@RequestParam(value="gameID") Long gameID)
 				throws GameNotExistException {
 			List<Player> players = gManager.getPlayers(gameID) ;
 			return players;
 		}
 	    
-	    @RequestMapping(method=RequestMethod.POST , value="/deck")
+	    @RequestMapping(method=RequestMethod.POST , value="/game/deck")
 	    public Long createDeck() throws DeckNotExistException{
 	    	Long deckID = gManager.createDeck() ;
 	    	return deckID ;
 	    }
 
-		@RequestMapping(method=RequestMethod.GET , value="/game")
-		public String[] UndealtCardCount(@RequestParam(value="gameID") Long gameID, @RequestParam(value="deckID") Long deckID)
+		@RequestMapping(method=RequestMethod.GET , value="/game/{gameID}/card")
+		public String[] UndealtCardCount(@PathVariable(value="gameID") Long gameID, @RequestParam(value="deckID") Long deckID)
 				throws GameNotExistException, DeckNotExistException {
 			String[] cards = gManager.UndealtCardCount(gameID, deckID) ;
 			return cards;
 		}
 
-		@RequestMapping(method=RequestMethod.GET , value="/game")
-		public Map<Suit, int[]> undealtCardCount(@RequestParam(value="gameID") Long gameID)
+		@RequestMapping(method=RequestMethod.GET , value="/game/{gameID}")
+		public Map<Suit, int[]> undealtCardCount(@PathVariable(value="gameID") Long gameID)
 				throws GameNotExistException {
 			Map<Suit, int[]> cards = gManager.UndealtCardCount(gameID) ;
 			return cards;
 		}
 	    
-	    @RequestMapping(method=RequestMethod.PUT , value="/game")
-	    public String shuffleADeck(@RequestParam(value="gameID") Long gameID, @RequestParam(value="deckID") Long deckID)
+	    @RequestMapping(method=RequestMethod.PUT , value="/game/{gameID}/deck/{deckID}/shuffle")
+	    public String shuffleADeck(@PathVariable(value="gameID") Long gameID, @PathVariable(value="deckID") Long deckID)
 				throws DeckNotExistException, GameNotExistException {
 	    	gManager.shuffle(gameID, deckID);
 	    	return SUCCESSFUL_SHUFFLE ;
